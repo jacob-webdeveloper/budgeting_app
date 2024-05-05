10aff9379eced9a18a19479ec390f957faa1c98e
jacob-webdeveloper <kochergenjacob@gmail.com>
2024-05-05 16:18:22 +0930

    tested and working menu, added functions file

--- main.py
+++ main.py
@@ -17,5 +17,20 @@ choice = ""
 while choice != "6":
     choice = create_menu()
 
-    if choice == 1:
-        print
+    if (choice == "1"):
+        print("You have selected 1")
+    elif choice == "2":
+        print("You have selected 2")
+    elif choice == "3":
+        print("You have selected 3")
+    elif choice == "4":
+        print("You have selected 4")
+    elif choice == "5":
+        print("You have selected 5")
+    elif choice == "6":
+        print("You have selected 6")
+    else:
+        print("Please only enter the options shown above.")
+
+
+print("Thankyou for using the budgeting app")
