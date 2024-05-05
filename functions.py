b557c86910f1123adb5ae7116c096ba796dec2b6
jacob-webdeveloper <kochergenjacob@gmail.com>
2024-05-05 19:01:38 +0930

    added extra option, created view goal function '

--- functions.py
+++ functions.py
@@ -7,6 +7,8 @@ def add_goal(filename):
         writer = csv.writer(f)
         writer.writerow([goal, amount])
 
+
+
 def goal_progress(filename):
     with open("goals.csv", "r") as f:
         reader = csv.reader(f)
@@ -15,22 +17,30 @@ def goal_progress(filename):
             print(f"Your {row[0]} has {row[1]} dollars remaining")
     
     goal_name = input("Enter the goal that you want to add progress towards: ")
-    amount_progress = input(f"Enter the amount that you want to put towards {goal_name}: ")
+    amount_progress = int(input(f"Enter the amount that you want to put towards {goal_name}: "))
     
     goal_lists = []
     with open("goals.csv", "r") as f:
         reader = csv.reader(f)
         for row in reader:
-            
             if goal_name != row[0]:
                 goal_lists.append(row)
             else:
-                goal_name == row[0]
-                original_value = (row[0][1])
+                original_value = int(row[1])
                 new_value = original_value - amount_progress
+                goal_lists.append([row[0], int(new_value)])
 
-                goal_lists.append(row[0], new_value)
-            
+    with open(filename, "w") as f:
+        writer = csv.writer(f)
+        writer.writerows(goal_lists)
+
+
+def goal_view(filename):
+    with open("goals.csv", "r") as f:
+        reader = csv.reader(f)
+        reader.__next__()
+        for row in reader:
+            print(f"You have ${row[1]} remaining in you {row[0]} goal.")
 
 
     
--- main.py
+++ main.py
@@ -7,17 +7,18 @@ print("Welcome to your budgeting app")
 def create_menu():
     print("1. Enter 1 to set a savings goal")
     print("2. Enter 2 to add progress towards a savings goal")
-    print("3. Enter 3 to add expenses")
-    print("4. Enter 4 to remove expenses")
-    print("5. Enter 5 to view expenses")
-    print("6. Enter 6 to exit")
+    print("3. Enter 3 to view savings goals")
+    print("4. Enter 4 to add expenses")
+    print("5. Enter 5 to remove expenses")
+    print("6. Enter 6 to view expenses")
+    print("7. Enter 7 to exit")
 
     user_choice = input("Enter your selection: ")
     return user_choice
 
 choice = ""
 
-while choice != "6":
+while choice != "7":
     choice = create_menu()
 
     if (choice == "1"):
@@ -33,7 +34,7 @@ while choice != "6":
         else:
             (os.path.isfile("goals.csv"))
             goal_progress("goals.csv")
-            
+
     elif choice == "3":
         print("You have selected 3")
     elif choice == "4":
@@ -42,6 +43,8 @@ while choice != "6":
         print("You have selected 5")
     elif choice == "6":
         print("You have selected 6")
+    elif choice == "7":
+        print("You have selected 7")
     else:
         print("Please only enter the options shown above.")
 
